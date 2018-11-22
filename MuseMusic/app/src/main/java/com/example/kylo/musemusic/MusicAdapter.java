a558d653b8ed5dc428a132fcd9e988ba05b7f2a4
EmilyPascua <epascuacollege@gmail.com>
2018-11-21 23:20:42 -0800

    Music is now playable, glitch with playing two songs

--- MuseMusic/app/src/main/java/com/example/kylo/musemusic/MusicAdapter.java
+++ MuseMusic/app/src/main/java/com/example/kylo/musemusic/MusicAdapter.java
@@ -2,16 +2,23 @@ package com.example.kylo.musemusic;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.Image;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicHolder>{
@@ -49,11 +56,15 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicHolder>
         TextView trackName;
         TextView trackArtist;
         TextView previewUrl;
-//        track_artist
+        Button playButton;
+        MediaPlayer mediaPlayer = new MediaPlayer();
+
+        //        track_artist
 //                track_name
 //        album_url
         public MusicHolder(View itemView) {
             super(itemView);
+            playButton = (Button) itemView.findViewById(R.id.play_button);
             albumUrl = (ImageView) itemView.findViewById(R.id.album_url);
             trackName = (TextView) itemView.findViewById(R.id.track_name);
             trackArtist = (TextView) itemView.findViewById(R.id.track_artist);
@@ -62,18 +73,39 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicHolder>
 
         void bind(final int listIndex) {
             Picasso.with(mContext).load(mTrack.get(listIndex).getAlbumUrl()).into(albumUrl);
+            playButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(!mediaPlayer.isPlaying()){
+                        mediaPlayer.start();
+                        playButton.setBackgroundResource(R.drawable.ic_pause_black_24dp);
+                    }else{
+                        mediaPlayer.pause();
+                        playButton.setBackgroundResource(R.drawable.ic_play_arrow_black_24dp);
+                    }
+                }
+            });
+
             trackName.setText(mTrack.get(listIndex).getTrackName());
             trackArtist.setText(mTrack.get(listIndex).getTrackArtist());
             previewUrl.setText(mTrack.get(listIndex).getPreviewUrl());
             itemView.setOnClickListener(this);
         }
 
+        void createMediaPlayer(String track, int playBack){
+            try{
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mediaPlayer.setDataSource(track);
+                mediaPlayer.prepare(); // might take long! (for buffering, etc)
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
         @Override
         public void onClick(View view) {
-//            String urlString = mTrack.get(getAdapterPosition()).getUrl();
-//            Intent intent = new Intent(mContext, WebActivity.class);
-//            intent.putExtra("urlString", urlString);
-//            mContext.startActivity(intent);
+//            mediaPlayer.start();
         }
     }
 }
