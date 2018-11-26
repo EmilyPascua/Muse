b10dc10858cf8440d025e8cc69e23bfe0e35b0d9
Marco Aguilar <aguilaramarco@yahoo.com>
2018-11-25 18:08:24 -0800

    Added fade in/out animation when switching between activities in BottomNavigationViewHelper

--- app/src/main/java/com/cafemanager/musefoundation/Home/HomeActivity.java
+++ app/src/main/java/com/cafemanager/musefoundation/Home/HomeActivity.java
@@ -51,7 +51,7 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
         BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, this, bottomNavigationViewEx);
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
--- app/src/main/java/com/cafemanager/musefoundation/Share/ShareActivity.java
+++ app/src/main/java/com/cafemanager/musefoundation/Share/ShareActivity.java
@@ -31,7 +31,7 @@ public class ShareActivity extends AppCompatActivity{
         Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
         BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, this, bottomNavigationViewEx);
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
