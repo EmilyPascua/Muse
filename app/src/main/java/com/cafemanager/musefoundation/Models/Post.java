e88b72eb6c6802e04437e6341dd44ecf150de035
Marco Aguilar <aguilaramarco@yahoo.com>
2018-12-01 11:06:37 -0800

    Works with Firebase to get posts count and populate recyclerview

---
+++ app/src/main/java/com/cafemanager/musefoundation/Models/Post.java
@@ -0,0 +1,64 @@
+package com.cafemanager.musefoundation.Models;
+
+public class Post {
+
+    /**
+        Not sure if all instance variables should hold Strings.
+        This is temporary.
+     */
+    private String mArtistName;
+    private String mSongName;
+    private String mAlbumImgUrl;
+    private String mSongPreview;
+    private String mDescription;
+
+    public Post(){}
+
+    public Post(String artistName, String songName, String albumImgUrl, String songPreview, String description) {
+        mArtistName = artistName;
+        mSongName = songName;
+        mAlbumImgUrl = albumImgUrl;
+        mSongPreview = songPreview;
+        mDescription = description;
+    }
+
+    public String getArtistName() {
+        return mArtistName;
+    }
+
+    public void setArtistName(String artistName) {
+        mArtistName = artistName;
+    }
+
+    public String getSongName() {
+        return mSongName;
+    }
+
+    public void setSongName(String songName) {
+        mSongName = songName;
+    }
+
+    public String getAlbumImgUrl() {
+        return mAlbumImgUrl;
+    }
+
+    public void setAlbumImgUrl(String albumImgUrl) {
+        mAlbumImgUrl = albumImgUrl;
+    }
+
+    public String getSongPreview() {
+        return mSongPreview;
+    }
+
+    public void setSongPreview(String songPreview) {
+        mSongPreview = songPreview;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+}
--- app/src/main/java/com/cafemanager/musefoundation/Utils/MusicAdapter.java
+++ app/src/main/java/com/cafemanager/musefoundation/Utils/MusicAdapter.java
@@ -5,26 +5,34 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.cafemanager.musefoundation.Models.Post;
 import com.cafemanager.musefoundation.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicHolder>{
 
     Context mContext;
-    private ArrayList<String> tempList = new ArrayList<>();
+//    private ArrayList<String> tempList = new ArrayList<>();
+    private List<Post> mPosts;
 
     // Pass in any necessary data to constructor so Adapter can attach the data to Music Player widget
     // mContext is used so that we can access LayoutInflater in onCreateViewHolder()
-    public MusicAdapter(Context context) {
+    public MusicAdapter(Context context, List<Post> posts) {
         mContext = context;
-        for(int i = 0; i < 10; i++) {
-            String tempString = "Temp string just to populate RecyclerView";
 
-            tempList.add(tempString);
-        }
+        mPosts = posts;
+
+//        for(int i = 0; i < 10; i++) {
+//            String tempString = "Temp string just to populate RecyclerView";
+//
+//            tempList.add(tempString);
+//        }
     }
 
     @Override
@@ -38,20 +46,41 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicHolder>
         return viewHolder;
     }
 
+
+
     @Override
     public void onBindViewHolder(MusicHolder holder, int position) {
 
+        // To make something apppear on the Album ImageView, you're gonna need to do
+        // a bit more work.
+
+        holder.mTrackNameTextView.setText(mPosts.get(position).getSongName());
+        holder.mTrackArtistTextView.setText(mPosts.get(position).getArtistName());
+
     }
+
+
 
     @Override
     public int getItemCount() {
-        return tempList.size();
+        return mPosts.size();
     }
+
+
 
     public class MusicHolder extends RecyclerView.ViewHolder{
 
+        private ImageView mAlbumImageView;
+        private TextView mTrackNameTextView;
+        private TextView mTrackArtistTextView;
+
         public MusicHolder(View itemView) {
             super(itemView);
+
+            mAlbumImageView = (ImageView) itemView.findViewById(R.id.album_image);
+            mTrackNameTextView = (TextView) itemView.findViewById(R.id.track_name);
+            mTrackArtistTextView = (TextView) itemView.findViewById(R.id.track_artist);
+
 
         }
     }
