15068ae0499017279599b646860a197a7d42b7bc
treblegni <treblegni.longevity@gmail.com>
2018-12-10 08:52:44 -0800

    Updated to Emily-MergedKevin

---
+++ app/src/main/java/com/cafemanager/musefoundation/Utils/SectionsStatePagerAdapter.java
@@ -0,0 +1,71 @@
+package com.cafemanager.musefoundation.Utils;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.cafemanager.musefoundation.Home.SectionsPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final HashMap<Fragment, Integer> mFragments = new HashMap<>();
+    private final HashMap<String, Integer> mFragmentNumbers = new HashMap<>();
+    private final HashMap<Integer, String> mFragmentNames = new HashMap<>();
+
+    public SectionsStatePagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    public void addFragment(Fragment fragment, String fragmentName){
+        mFragmentList.add(fragment);
+        mFragments.put(fragment, mFragmentList.size()-1);
+        mFragmentNumbers.put(fragmentName, mFragmentList.size());
+        mFragmentNames.put(mFragmentList.size()-1, fragmentName);
+    }
+
+
+    public Integer getFragmentName(String fragmentName){
+        if(mFragmentNumbers.containsKey(fragmentName)){
+            return mFragmentNumbers.get(fragmentName);
+        }
+        else{
+            return null;
+        }
+    }
+
+
+
+
+    public Integer getFragmentNumber(Integer fragmentNumber){
+        if(mFragmentNumbers.containsKey(fragmentNumber)){
+            return mFragmentNumbers.get(fragmentNumber);
+        }
+        else{
+            return null;
+        }
+    }
+
+    public Integer getFragmentNumber(Fragment fragment){
+        if(mFragmentNumbers.containsKey(fragment)){
+            return mFragmentNumbers.get(fragment);
+        }
+        else{
+            return null;
+        }
+    }
+}
